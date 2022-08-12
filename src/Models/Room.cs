a67e9f722620070bb0caf703606a419f10492bc6
keczanilles <illeskeczan99@gmail.com>
2022-08-12 12:37:07 +0200

    Created gitignore file, Student and Room model

--- src/Models/Room.cs
+++ src/Models/Room.cs
@@ -1,7 +1,27 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components.Web;
+
 namespace HogwartsHouses.Models
 {
     [System.Serializable]
     public class Room
     {
+        public int Id { get; set; }
+
+        private HashSet<Student> _students;
+
+        public int Capacity { get;}
+
+        public bool IsAvailable => _students.Count < Capacity;
+
+        public bool NeedRenovation { get; set; }
+
+        public Room(int id)
+        {
+            Id = id;
+            Capacity = 5;
+            NeedRenovation = false;
+            _students = new();
+        }
     }
 }
--- src/Models/Student.cs
+++ src/Models/Student.cs
@@ -1,7 +1,25 @@
+using System;
+using HogwartsHouses.Models.Types;
+
 namespace HogwartsHouses.Models
 {
     [System.Serializable]
     public class Student
     {
+        public string Name { get; set; }
+
+        public HouseType HouseType { get; set; }
+
+        public PetType PetType { get; set; }
+
+        public Student(string name, HouseType houseType, PetType petType)
+        {
+            Name = name;
+            HouseType = houseType;
+            PetType = petType;
+        }
+
+
+
     }
 }
